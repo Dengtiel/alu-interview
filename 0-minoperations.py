0fff80238ec958ae25a84b39f7285a0349bd5f87
Dengtiel <d.akol@alustudent.com>
2025-05-21 19:23:01 +0200

    initial

--- 0-minoperations.py
+++ 0-minoperations.py
@@ -3,6 +3,7 @@
 Module to determine the minimum number of operations to achieve exactly n H characters.
 """
 
+
 def minOperations(n):
     """
     Calculate the minimum number of operations (Copy All and Paste) to get n H characters.
@@ -15,14 +16,14 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n = n // divisor
         divisor += 1
-    
+
     return operations
